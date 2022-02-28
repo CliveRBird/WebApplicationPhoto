16abd1f8fea90812253aeecbfec304de5188c6ae
Clive87375 <42ayu75yx6le4jcdfq5gaikslh7xozxc66ifdn5oahco6aipy6za>
2022-02-28 17:19:51 +0000

    Made ammendments.

--- WebApplicationPhoto/PersonHelper.cs
+++ WebApplicationPhoto/PersonHelper.cs
@@ -3,10 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+using System.IO;
+
 namespace WebApplicationPhoto
 {
     public class PersonHelper
     {
+        private static string strConn;
+
+        static PersonHelper()
+        {
+            strConn = ConfigurationManager.ConnectionStrings["connstr_encrypted"].ConnectionString;
+        }
+
+        public static int Insert(Person p)
+        {
+            int i = 0;
+
+            return i;
+
+        }
+
+        public static int Update(Person p)
+        {
+            int i = 0;
+
+            return i;
+        }
+
+        public static int Delete(int p)
+        {
+            int i = 0;
+
+            return i;
+
+        }
+
+        public static List<Person> GetAll()
+        {
+            List<Person> persons = new List<Person>();
+
+            return persons;
+        }
+
+        public static Person GetByID(int personid)
+        {
+            Person p = new Person();
+
+            return p;
+        }
+
     }
 
 
