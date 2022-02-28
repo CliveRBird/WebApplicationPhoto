8717559f79030721ad9ee0f70e67765889fcb550
Clive87375 <42ayu75yx6le4jcdfq5gaikslh7xozxc66ifdn5oahco6aipy6za>
2022-02-28 17:39:32 +0000

    Half written PersonHelper methods

--- WebApplicationPhoto/PersonHelper.cs
+++ WebApplicationPhoto/PersonHelper.cs
@@ -24,6 +24,26 @@ namespace WebApplicationPhoto
         {
             int i = 0;
 
+            SqlConnection cnn = new SqlConnection(strConn);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = cnn;
+            cmd.CommandText = "insert into person([NationalInsuranceNumber],[FirstName],[LastName],[Photo]) values (@NationalInsuranceNumber, @FirstName, @LastName, @photo)";
+
+            SqlParameter NationalInsuranceNumber = new SqlParameter("@NationalInsuranceNumber", p.NationalInsuranceNumber);
+            SqlParameter FirstName = new SqlParameter("@FirstName", p.FirstName);
+            SqlParameter LastName = new SqlParameter("@LastName", p.LastName);
+            SqlParameter photo = new SqlParameter("@photo", SqlDbType.VarBinary);
+            photo.Value = p.Photo;
+            cmd.Parameters.Add(NationalInsuranceNumber);
+            cmd.Parameters.Add(FirstName);
+            cmd.Parameters.Add(LastName);
+            cmd.Parameters.Add(photo);
+
+            cnn.Open();
+            i = cmd.ExecuteNonQuery();
+            cnn.Close();
+
             return i;
 
         }
@@ -31,6 +51,34 @@ namespace WebApplicationPhoto
         public static int Update(Person p)
         {
             int i = 0;
+
+            SqlConnection cnn = new SqlConnection(strConn);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = cnn;
+            cmd.CommandText = "update person set NationalInsuranceNumber=@NationalInsuranceNumber,FirstName=@FirstName,Lastname=@LastName,photo.write(@photo, @offset, @length) where personid = @personid";
+
+            SqlParameter NationalInsuranceNumber = new SqlParameter("@NationalInsuranceNumber", p.NationalInsuranceNumber);
+            SqlParameter FirstName = new SqlParameter("@FirstName", p.FirstName);
+            SqlParameter LastName = new SqlParameter("@LastName", p.LastName);
+            SqlParameter photo = new SqlParameter("@photo", SqlDbType.VarBinary);
+            photo.Value = p.Photo;
+            SqlParameter offset = new SqlParameter("@offset", SqlDbType.BigInt);
+            offset.Value = 0;
+            SqlParameter length = new SqlParameter("@length", p.PhotoData.Length);
+            SqlParameter personid = new SqlParameter("@personid", p.PersonID);
+
+            cmd.Parameters.Add(NationalInsuranceNumber);
+            cmd.Parameters.Add(FirstName);
+            cmd.Parameters.Add(LastName);
+            cmd.Parameters.Add(photo);
+            cmd.Parameters.Add(offset);
+            cmd.Parameters.Add(length);
+            cmd.Parameters.Add(personid);
+
+            cnn.Open();
+            i = cmd.ExecuteNonQuery();
+            cnn.Close();
 
             return i;
         }
